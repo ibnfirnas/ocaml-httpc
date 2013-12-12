2c6b40d4a4de8799ad9abfff40787d901b98a7a8
Siraaj Khandkar <siraaj@khandkar.net>
2013-12-11 22:05:38 -0500

    Add TODO note to handle specific exit codes.

--- lib/curl.ml
+++ lib/curl.ml
@@ -111,6 +111,7 @@ struct
     | R.Ok proc ->
       match P.wait proc with
       | (R.Ok _) as ok                  -> ok
+      (* TODO: Convert (some) curl exit codes to variants *)
       | R.Error (P.Fail (code, stderr)) -> R.Error (code, stderr)
       | R.Error (P.Signal     _)        -> assert false
       | R.Error (P.Stop       _)        -> assert false
