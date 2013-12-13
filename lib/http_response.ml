fb96af7617dd903c1bc173fa5e9eff44c8810d53
Siraaj Khandkar <siraaj@khandkar.net>
2013-12-13 01:05:44 -0500

    s/status_msg/reason_phrase/g

--- lib/http_response.ml
+++ lib/http_response.ml
@@ -1,13 +1,13 @@
 type http_vsn    = [ `Not_implemented ]
 type status_code = [ `Not_implemented ]
-type status_msg  = [ `Not_implemented ]
+type reason_phrase = [ `Not_implemented ]
 type headers     = [ `Not_implemented ]
 type body        = [ `Not_implemented ]
 
 type t =
   { http_vsn    : http_vsn
   ; status_code : status_code
-  ; status_msg  : status_msg
+  ; reason_phrase : reason_phrase
   ; headers     : headers
   ; body        : body
   ; raw         : string
@@ -16,7 +16,7 @@ type t =
 let of_string s =
   { http_vsn    = `Not_implemented
   ; status_code = `Not_implemented
-  ; status_msg  = `Not_implemented
+  ; reason_phrase = `Not_implemented
   ; headers     = `Not_implemented
   ; body        = `Not_implemented
   ; raw         = s
--- lib/http_response.mli
+++ lib/http_response.mli
@@ -1,13 +1,13 @@
 type http_vsn    = [ `Not_implemented ]
 type status_code = [ `Not_implemented ]
-type status_msg  = [ `Not_implemented ]
+type reason_phrase = [ `Not_implemented ]
 type headers     = [ `Not_implemented ]
 type body        = [ `Not_implemented ]
 
 type t =
   { http_vsn    : http_vsn
   ; status_code : status_code
-  ; status_msg  : status_msg
+  ; reason_phrase : reason_phrase
   ; headers     : headers
   ; body        : body
   ; raw         : string
