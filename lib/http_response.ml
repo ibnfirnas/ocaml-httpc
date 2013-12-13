7139df876a5332f4cb751ec044162aa530c9a3ef
Siraaj Khandkar <siraaj@khandkar.net>
2013-12-13 01:08:42 -0500

    Align fields.

--- lib/http_response.ml
+++ lib/http_response.ml
@@ -1,23 +1,23 @@
-type http_vsn    = [ `Not_implemented ]
-type status_code = [ `Not_implemented ]
+type http_vsn      = [ `Not_implemented ]
+type status_code   = [ `Not_implemented ]
 type reason_phrase = [ `Not_implemented ]
-type headers     = [ `Not_implemented ]
-type body        = [ `Not_implemented ]
+type headers       = [ `Not_implemented ]
+type body          = [ `Not_implemented ]
 
 type t =
-  { http_vsn    : http_vsn
-  ; status_code : status_code
+  { http_vsn      : http_vsn
+  ; status_code   : status_code
   ; reason_phrase : reason_phrase
-  ; headers     : headers
-  ; body        : body
-  ; raw         : string
+  ; headers       : headers
+  ; body          : body
+  ; raw           : string
   }
 
 let of_string s =
-  { http_vsn    = `Not_implemented
-  ; status_code = `Not_implemented
+  { http_vsn      = `Not_implemented
+  ; status_code   = `Not_implemented
   ; reason_phrase = `Not_implemented
-  ; headers     = `Not_implemented
-  ; body        = `Not_implemented
-  ; raw         = s
+  ; headers       = `Not_implemented
+  ; body          = `Not_implemented
+  ; raw           = s
   }
--- lib/http_response.mli
+++ lib/http_response.mli
@@ -1,16 +1,16 @@
-type http_vsn    = [ `Not_implemented ]
-type status_code = [ `Not_implemented ]
+type http_vsn      = [ `Not_implemented ]
+type status_code   = [ `Not_implemented ]
 type reason_phrase = [ `Not_implemented ]
-type headers     = [ `Not_implemented ]
-type body        = [ `Not_implemented ]
+type headers       = [ `Not_implemented ]
+type body          = [ `Not_implemented ]
 
 type t =
-  { http_vsn    : http_vsn
-  ; status_code : status_code
+  { http_vsn      : http_vsn
+  ; status_code   : status_code
   ; reason_phrase : reason_phrase
-  ; headers     : headers
-  ; body        : body
-  ; raw         : string
+  ; headers       : headers
+  ; body          : body
+  ; raw           : string
   }
 
 val of_string : string -> t
