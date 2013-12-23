b40a4771ce43dbe0448f076111a0201be91e8f15
Siraaj Khandkar <siraaj@khandkar.net>
2013-12-22 23:33:17 -0500

    Hide internal module.

--- lib/httpc.ml
+++ lib/httpc.ml
@@ -1,6 +1,5 @@
 module Header   = Httpc_header
 module Method   = Httpc_method
-module Process  = Httpc_process
 module Protocol = Httpc_protocol
 module Request  = Httpc_request
 module Response = Httpc_response
